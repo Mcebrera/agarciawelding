6436afd69795e3e60427a1331b6ae2e3c4793528
michael <michael.cebrera@gmail.com>
2020-03-08 15:02:00 -0700

    jquery isotope filter system for picture gallery

--- js/grayscale.js
+++ js/grayscale.js
@@ -39,4 +39,32 @@
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
 
+  var $container = $('.portfolioContainer');
+  $container.isotope({
+      filter: '*',
+      animationOptions: {
+          duration: 750,
+          easing: 'linear',
+          queue: false
+      }
+  });
+
+  $('.portfolioFilter a').click(function() {
+      $('.portfolioFilter .current').removeClass('current');
+      $(this).addClass('current');
+
+      var selector = $(this).attr('data-filter');
+
+      $container.isotope({
+        filter: selector,
+        animationOptions: {
+          duration: 750,
+          easing: 'linear',
+          queue: false
+        }
+       });
+
+       return false;
+  });
+
 })(jQuery); // End of use strict
